45e49c9f2e1ddabe0c5918b25769775d603ae805
Vanshika521 <159027194+Vanshika521@users.noreply.github.com>
2025-06-26 23:02:22 +0530

    First Commit

---
+++ app/src/main/java/com/android_development/datefinder/abc.kt
@@ -0,0 +1,14 @@
+package com.android_development.datefinder
+
+class abc {
+
+    companion object{
+
+        @JvmStatic
+
+        fun main(args: Array<String>){
+            println("Hello ! This is My First Project")
+        }
+    }
+
+}
